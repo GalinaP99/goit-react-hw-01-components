c980f25acbef10b53147ad0f13afcbe0e2043754
Galina Pankevych <gln99@ukr.net>
2021-07-11 16:34:36 +0300

    add folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import './App.css';
 import Profile from './components/Profile';
 import user from './data/user.json';
+//import statisticalData from './data/statistics-data.json';
+//import friends from 'путь/к/friends.json';
+//import transactions from 'путь/к/transactions.json';
 function App() {
   return (
     <div className="App">
       <Profile
-       // {...user}
+        // {...user}
         name={user.name}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
     </div>
+    /*<Statistics title="Upload stats" stats={statisticalData} />;
+<Statistics stats={statisticalData} />;
+<FriendList friends={friends} />,
+<TransactionHistory items={transactions} />;*/
   );
 }
 
 export default App;
-
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,9 @@
+/*<ul class="friend-list">
+  <!-- Произвольное кол-во FriendListItem, в зависимости от кол-ва объектов в массиве -->
+</ul>*/
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="" width="48" />
+  <p class="name"></p>
+</li>;
